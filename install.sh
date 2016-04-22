7157086314a01c6e325f6d80ec41b9b0ec2bbfdd
Jhuni0123 <whdgnsdl887@naver.com>
2016-04-22 15:46:18 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+#install vim plugins
+echo copy vimrc...
+cp .vimrc $HOME/.vimrc
+echo copy vim colorscheme...
+cp -r color $HOME/.vim/
+echo install vim plugins...
+vim +PluginInstall +qall
+
+#install oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+#install zplug
+git clone https://github.com/b4b4r07/zplug ~/.zplug
+
+#install zsh plugins
+cp .zshrc $HOME/.zshrc
+chsh -s $(which zsh)
