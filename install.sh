ead2647d93c5775ababeafb72509a79488e11849
Jhuni0123 <whdgnsdl887@gmail.com>
2021-01-10 16:43:06 +0900

    Update install script

--- install.sh
+++ install.sh
@@ -4,17 +4,15 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# install oh-my-zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# install zinit
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
 # link force dotfiles
 ln -f .vimrc ~/.vimrc
 ln -f .zshrc ~/.zshrc
 ln -f .gitconfig ~/.gitconfig
-
-# install zplug
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+ln -s .zsh ~/.zsh
 
 vim +PlugInstall +qall
 
-sudo chsh -s $(which zsh)
+# sudo chsh -s $(which zsh)
