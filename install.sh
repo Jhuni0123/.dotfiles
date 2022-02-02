14575127329e71d9cc0657a74234bfe78190d6be
Jhuni0123 <whdgnsdl887@gmail.com>
2022-02-02 17:16:52 +0900

    Make vimrc point here

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
 # link force dotfiles
-ln -f .vimrc ~/.vimrc
 ln -f .zshrc ~/.zshrc
 ln -f .gitconfig ~/.gitconfig
 ln -s $PWD/.zsh ~/.zsh
 
+echo "source $(pwd)/vimrc/basic.vim" > ~/.vimrc
 vim +PlugInstall +qall
 
 # sudo chsh -s $(which zsh)
