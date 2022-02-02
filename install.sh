c3be5a70545aba2132c4f5dc64cae300f13a8d62
Jhuni0123 <whdgnsdl887@gmail.com>
2022-02-02 18:51:28 +0900

    Update installation script

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
+echo "Connect Git config..."
 git config --global include.path "${PWD}/gitconfig_global"
 
+echo "Connect zsh config..."
 echo "export DOTFILES_HOME=${PWD}
 source ${PWD}/zshrc/basic.zsh
 " > ~/.zshrc
 
-echo "source ${PWD}/vimrc/basic.vim" > ~/.vimrc
-vim +PlugInstall +qall
+echo ''
+echo "Change your default shell to zsh"
+echo '  $ chsh -s $(which zsh)'
+echo '  or'
+echo "  $ echo 'exec zsh' >> ~/.bash_profile"
+echo ''
 
-# sudo chsh -s $(which zsh)
+echo "Connect vim config..."
+echo "source ${PWD}/vimrc/basic.vim" > ~/.vimrc
