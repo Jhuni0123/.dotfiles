45affc8180e0cb9086653dce67c38dcfb9750255
Jonghun Park <whdgnsdl887@gmail.com>
2022-02-04 22:41:44 +0900

    Add direnv config

--- install.sh
+++ install.sh
@@ -24,3 +24,7 @@ echo "source ${PWD}/vimrc/basic.vim" >> ~/.vimrc
 # tmux
 echo "Connect tmux config..."
 echo "source-file ${PWD}/tmux.conf" >> ~/.tmux.conf
+
+# direnv
+echo "Connect direnv config..."
+echo "source ${PWD}/direnvrc" >> ~/.direnvrc
