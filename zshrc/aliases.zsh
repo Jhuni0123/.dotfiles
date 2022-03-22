d3ba17ad1e174a58d3900d77af18bcd20236e92b
Jhuni0123 <whdgnsdl887@gmail.com>
2022-03-22 16:40:53 +0900

    [zshrc] Fix checking command

--- zshrc/aliases.zsh
+++ zshrc/aliases.zsh
@@ -5,7 +5,7 @@ alias cp="cp -i"
 alias sudo="sudo -H"
 alias venv="echo \$VIRTUAL_ENV"
 
-if [[ $OS == "macos" && $commands[gls] ]]; then
+if [[ $OS == "macos" ]] && type gls &> /dev/null; then
     alias ls="gls --color"
 else
     alias ls="ls -G --color"
